338ba0d7855d817f638ca636bba1e56771cf6485
Godfrey Dsouza <godfrey.dsouza@morgansindall.com>
2022-02-03 22:05:36 +0000

    saving changes

---
+++ ConsoleApp1/ConsoleApp1/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Constants
+    {
+        enum Days { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+        static void Main(string[] args)
+        {
+            const double pi = 3.121321312323;
+            var dataType = typeof(Days);
+            Console.WriteLine("Pi Type : " + pi.GetType());
+            Console.WriteLine("Circumference: " + (pi * 3));
+            Console.WriteLine("First day is " + Days.Sat);
+        }
+    }
+}
+
